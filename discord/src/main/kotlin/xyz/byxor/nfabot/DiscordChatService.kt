1a59e6f8734776897244c8dbcf97d6ee7f94b5ce
byxor <brandnewbyxor@gmail.com>
2019-12-27 13:16:05 +0000

    Add channels to discord's ChatService implementation

--- discord/src/main/kotlin/xyz/byxor/nfabot/DiscordChatService.kt
+++ discord/src/main/kotlin/xyz/byxor/nfabot/DiscordChatService.kt
@@ -1,19 +1,22 @@
 package xyz.byxor.nfabot
 
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.entities.TextChannel
 import xyz.byxor.nfabot.chat.ChatService
 
 class DiscordChatService(private val discordApi: JDA) : ChatService {
 
-    private val generalChannel: TextChannel
-
     init {
         discordApi.awaitReady()
-        generalChannel = discordApi.getTextChannelsByName("general", true)[0]
     }
 
-    override fun sendMessage(message: String) {
-        generalChannel.sendMessage(message).queue()
+    override fun sendMessage(message: String, channel: String?) {
+        val channelObject = when(channel) {
+            null -> getChannel("general")
+            else -> getChannel(channel)
+        }
+
+        channelObject.sendMessage(message).queue()
     }
+
+    private fun getChannel(name: String) = discordApi.getTextChannelsByName(name, true)[0]
 }
